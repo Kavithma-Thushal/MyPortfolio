44ed2507f03209eae6d87fa400c20a6987b7c1e8
Kavithma Thushal <125787087+Thushal2001@users.noreply.github.com>
2023-10-22 10:36:28 +0530

    New Background Video Added

--- assets/js/dark_mode.js
+++ assets/js/dark_mode.js
@@ -53,7 +53,7 @@ $('.theme-checkbox').click(function () {
         $('.menu ul li a').css('color', 'white');
 
         /*home*/
-        $('main > section:nth-child(1)').css('background-color', 'black');
+        $('main > section:nth-child(1)').css('background', 'url(assets/img/background.mp4) no-repeat');
         $('main > section:nth-child(1) > section > div:first-child > aside:nth-child(1) > h1:nth-child(2) > span').css('color', 'white');
         $('main > section:nth-child(1) > section > div:first-child > aside:nth-child(1) > h1:nth-child(3)').css('color', 'white');
         $('main > section:nth-child(1) > section > div:first-child > aside:nth-child(1) > section:nth-child(5) > h3').css('color', 'white');
@@ -61,7 +61,7 @@ $('.theme-checkbox').click(function () {
         $('main > section:nth-child(1) > section > div:first-child > aside:nth-child(1) > section:nth-child(5) > nav:nth-child(2) > div > a > i').css('color', 'black');
 
         /*about*/
-        $('main > section:nth-child(2)').css('background-color', 'black');
+        $('main > section:nth-child(2)').css('background', 'url(assets/img/background.mp4) no-repeat');
         $('main > section:nth-child(2) > section:first-child > h1').css('color', 'white');
         $('main > section:nth-child(2) > section:nth-child(3) > div:first-child > aside:nth-child(2) > h2:first-child').css('color', 'white');
         $('main > section:nth-child(2) > section:nth-child(3) > div:first-child > aside:nth-child(2) > p:nth-child(2), p:nth-child(3)').css('color', 'white');
@@ -71,23 +71,23 @@ $('.theme-checkbox').click(function () {
         $('main > section:nth-child(2) > section:nth-child(3) > div:first-child > aside:nth-child(2) > div:nth-child(4) > #column2 > p > span').css('color', 'white');
 
         /*services*/
-        $('main > section:nth-child(3)').css('background-color', 'black');
+        $('main > section:nth-child(3)').css('background', 'url(assets/img/background.mp4) no-repeat');
         $('main > section:nth-child(3) > section:first-child > h1').css('color', 'white');
 
         /*skills*/
-        $('main > section:nth-child(4)').css('background-color', 'black');
+        $('main > section:nth-child(4)').css('background', 'url(assets/img/background.mp4) no-repeat');
         $('main > section:nth-child(4) > section:first-child > h1').css('color', 'white');
 
         /*gallery*/
-        $('main > section:nth-child(5)').css('background-color', 'black');
+        $('main > section:nth-child(5)').css('background', 'url(assets/img/background.mp4) no-repeat');
         $('main > section:nth-child(5) > section:first-child > h1').css('color', 'white');
 
         /*projects*/
-        $('main > section:nth-child(6)').css('background-color', 'black');
+        $('main > section:nth-child(6)').css('background', 'url(assets/img/background.mp4) no-repeat');
         $('main > section:nth-child(6) > section:first-child > h1').css('color', 'white');
 
         /*contacts*/
-        $('main > section:nth-child(7)').css('background-color', 'black');
+        $('main > section:nth-child(7)').css('background', 'url(assets/img/background.mp4) no-repeat');
         $('main > section:nth-child(7) > section:first-child > h1').css('color', 'white');
         $('main > section:nth-child(7) > section:nth-child(2) > section:first-child > h4:nth-child(2)').css('color', 'white');
         $('main > section:nth-child(7) > section:nth-child(2) > section:nth-child(2) > div:first-child > div > h4:nth-child(2)').css('color', 'white');
